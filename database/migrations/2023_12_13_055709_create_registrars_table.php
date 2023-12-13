fd0314661a8eab1b13dabcacdefafb9d819e6c26
a.adonin <a.adonin@parusgroup.ru>
2023-12-13 09:59:57 +0400

    Таблица "Регистраторы"

---
+++ database/migrations/2023_12_13_055709_create_registrars_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('registrars', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+
+            $table->string('title');
+            $table->text('info')->nullable();
+            $table->string('url')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('registrars');
+    }
+};
