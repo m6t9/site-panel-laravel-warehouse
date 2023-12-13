fd40bf26176c83978d74d97efb3ab4aed93a8ffc
a.adonin <a.adonin@parusgroup.ru>
2023-12-13 10:01:32 +0400

    Таблица "Хостинги"

---
+++ database/migrations/2023_12_13_060005_create_hostings_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('hostings', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+
+            $table->string('title');
+            $table->text('info')->nullable();
+            $table->string('url')->nullable();
+            $table->string('account')->nullable();
+            $table->date('paid_to')->nullable()->index();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('hostings');
+    }
+};
